04d3014e3b5354275bbffcd4c65dbec506622a68
coder-jkb <jkb17codes@gmail.com>
2022-03-12 20:11:46 +0530

    card ui bug fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Sports from "./pages/Sports";
+import Rewards from "./components/Rewards";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sport" element={<Sports />} />
+        <Route path="/rewards" element={<Rewards />} />
       </Routes>
     </>
   );
--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Card from './Card'
-// import '../rewards.css'
 
 function Rewards() {
   var fullDate = new Date()
   var today = fullDate.toLocaleDateString('en-UK');
   var nextDate = new Date(new Date().getTime()+(7*24*60*60*1000)).toLocaleDateString('en-UK')
   return (
-    <div className="flex">
-      <Card head="Congrats!" msg="For completing a 7 days streak" date={today}/>
-      <Card head="Next Reward" msg="Complete a 14 days streak by" date={nextDate}/>
+    <div className="flex-col ml-[10vh]">
+      <div className="flex">
+        <Card head="Congrats!" msg="For completing a 7 days streak" date={today}/>
+        <Card head="Next Reward" msg="Complete a 14 days streak by" date={nextDate}/>
+      </div>
+      {/* parting line */}
+      <div className="ml-5 w-[95%] h-[2px] my-1 bg-gray-300 rounded-full"></div>
+      <div className='mx-10 text-xl font-bold'>
+        Previous Rewards
+      </div>
+      <div className="flex">
+        <Card head="Congrats!" msg="For completing a 7 days streak" date={today}/>
+        <Card head="Next Reward" msg="Complete a 14 days streak by" date={nextDate}/>
+      </div>
     </div>
   );
 }
