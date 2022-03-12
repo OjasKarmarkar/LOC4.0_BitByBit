dd277528d3f153f53818f27901009afe835c6428
PrachetShah <prachetshah25@gmail.com>
2022-03-12 16:29:09 +0530

    sports page basic layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Sports from "./pages/Sports";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-            <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sport" element={<Sports />} />
       </Routes>
     </>
   );
