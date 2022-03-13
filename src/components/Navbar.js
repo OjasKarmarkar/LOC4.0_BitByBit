b632afe48e67cd97c8facd927ce289f67a077cb7
PrachetShah <prachetshah25@gmail.com>
2022-03-13 10:16:45 +0530

    name changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,48 +7,92 @@ import { FiLogOut } from "react-icons/fi";
 import { MdFaceRetouchingNatural } from "react-icons/md";
 
 function Navbar() {
-
-  const [icon, setIcon] = useState("profile")
+  const [icon, setIcon] = useState("profile");
   const handleClick = (menuIcon) => {
-    setIcon(menuIcon)
-  }
+    setIcon(menuIcon);
+  };
   return (
     <div className="fixed h-full w-[10vh] border-2 z-10">
       <div className="w-full h-[10%] flex justify-center items-center">
-        <Link to={"/"} onClick={()=>{handleClick("profile")}}>
+        <Link
+          to={"/"}
+          onClick={() => {
+            handleClick("profile");
+          }}
+        >
           <MdFaceRetouchingNatural size={55} className="text-blue-700" />
         </Link>
       </div>
       <div className="w-full h-[80%] flex flex-col justify-start items-center pt-10 space-y-5">
         <div className="relative h-16">
-          <Link to={"/"} onClick={()=>{handleClick("home")}}>
-            <AiFillHome size={30} className={`peer text-${icon==="home"?"blue-600":"gray-400"}`} />
+          <Link
+            to={"/"}
+            onClick={() => {
+              handleClick("home");
+            }}
+          >
+            <AiFillHome
+              size={30}
+              className={`peer text-${
+                icon === "home" ? "blue-600" : "gray-400"
+              }`}
+            />
             <div className="absolute -top-1 left-[150%] scale-0 peer-hover:scale-100 transition-transform duration-150 origin-left bg-white rounded-xl p-2 font-semibold text-gray-800 drop-shadow-lg">
               Home
             </div>
           </Link>
         </div>
         <div className="relative h-16">
-          <Link to={"/rewards"} onClick={()=>{handleClick("rewards")}}>
-            <AiFillGift size={30} className={`peer text-${icon==="rewards"?"blue-600":"gray-400"}`} />
+          <Link
+            to={"/rewards"}
+            onClick={() => {
+              handleClick("rewards");
+            }}
+          >
+            <AiFillGift
+              size={30}
+              className={`peer text-${
+                icon === "rewards" ? "blue-600" : "gray-400"
+              }`}
+            />
             <div className="absolute -top-1 left-[150%] scale-0 peer-hover:scale-100 transition-transform duration-150 origin-left bg-white rounded-xl p-2 font-semibold text-gray-800 drop-shadow-lg">
               Rewards
             </div>
           </Link>
         </div>
         <div className="relative h-16">
-          <Link to={"/sport"} onClick={()=>{handleClick("sports")}}>
-            <MdOutlineSportsTennis size={30} className={`peer text-${icon==="sports"?"blue-600":"gray-400"}`} />
+          <Link
+            to={"/sport"}
+            onClick={() => {
+              handleClick("sports");
+            }}
+          >
+            <MdOutlineSportsTennis
+              size={30}
+              className={`peer text-${
+                icon === "sports" ? "blue-600" : "gray-400"
+              }`}
+            />
             <div className="absolute -top-1 left-[150%] scale-0 peer-hover:scale-100 transition-transform duration-150 origin-left bg-white rounded-xl p-2 font-semibold text-gray-800 drop-shadow-lg">
               Sports
             </div>
           </Link>
         </div>
         <div className="relative h-16">
-          <Link to={"/events"} onClick={()=>{handleClick("events")}}>
-            <BsFillCalendarEventFill size={30} className={`peer text-${icon==="events"?"blue-600":"gray-400"}`} />
+          <Link
+            to={"/events"}
+            onClick={() => {
+              handleClick("events");
+            }}
+          >
+            <BsFillCalendarEventFill
+              size={30}
+              className={`peer text-${
+                icon === "events" ? "blue-600" : "gray-400"
+              }`}
+            />
             <div className="absolute -top-1 left-[150%] scale-0 peer-hover:scale-100 transition-transform duration-150 origin-left bg-white rounded-xl p-2 font-semibold text-gray-800 drop-shadow-lg">
-              Description
+              Events
             </div>
           </Link>
         </div>
